8c3593cccb8ae3c2f5cbd7d7f68222dbf1e5daa2
Michael Adebanjo <ommicang@gmail.com>
2022-05-06 03:33:22 +0100

    +SPDthemeSND/Func

--- webTerra.js
+++ webTerra.js
@@ -6,10 +6,34 @@ lightBulb.onclick = ()=>{
   // lightBulb Icon change functionality
   if(document.body.classList.contains("dark-mode")){
     lightBulb.src = "./svgImages/darkModeSvg/sun.png";
+    musicPlay();
+
+
   } else {
     lightBulb.src= "./svgImages/darkModeSvg/light-mobile-ui-basic-lamp-dark-mode-svgrepo-com.svg";
+    musicStop();
+
+
   }
 
 }
 
 // DarkMode Feature ends Here
+
+// MusicPlay Int with LightBulb: Credits @Paul Becker(stack):
+
+function musicPlay() {
+    document.getElementById('ID').play();
+}
+
+function musicStop() {
+var reset = document.getElementById('ID');
+  reset.pause();
+  if (reset.pause()){
+    reset.play(); // Prev issue solved reset code credits @adeneo (Stack)
+  }
+    else{
+      reset.currentTime = 0;
+    }
+  }
+
